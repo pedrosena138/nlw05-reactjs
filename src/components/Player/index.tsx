dc2cf850cb26af211f559cec19e83c37e4757fa8
Pedro Sena <pmfsl@cin.ufpe.br>
2021-04-20 18:47:27 -0300

    add typescript

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.scss";
+
+export default function Player() {
+  return (
+    <div className={styles.container}>
+      <header>
+        <img src="/playing.svg" alt="tocando agora" />
+        <strong>Tocando agora</strong>
+      </header>
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+      <footer></footer>
+    </div>
+  );
+}
