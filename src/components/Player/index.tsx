4b731f81175fc801dad36c3ec62ff2cb20311c61
Pedro Sena <pmfsl@cin.ufpe.br>
2021-04-20 23:09:51 -0300

    add server.json

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -10,7 +10,32 @@ export default function Player() {
       <div className={styles.emptyPlayer}>
         <strong>Selecione um podcast para ouvir</strong>
       </div>
-      <footer></footer>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider} />
+          </div>
+          <span>00:00</span>
+        </div>
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="aleatório" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="anterior" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="tocar" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="próximo" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="repetir" />
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <h1>Index</h1>;
+class HomeProps {
+  episodes: object;
+}
+export default function Home({ episodes }: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p> {JSON.stringify(episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
